03526b931f16dfd084e4cc70019180237a23ba0b
Dexter Crago <jodieanddexter@gmail.com>
2023-10-09 10:54:42 -0700

    Write algorithm for game
    I added the algorithm I wrote for the rock paper scissors game as
    pseudocode to work from.

---
+++ javascript.js
@@ -0,0 +1,10 @@
+//Need a way to initialize game
+//Computer makes its choice randomly
+//Receive user input (choice)
+    //Must accept all spellings
+//Determine winner
+//Display who won
+//Need a way to play multiple times (five)
+//Need a system to keep track of scores
+//Determine overall winner
+//Display overall winner
