6cd935afcddc0561b046f9de142090fb36b0cdc5
Dexter Crago <jodieanddexter@gmail.com>
2023-10-09 11:07:06 -0700

    Create getComputerChoice function
    This function generates the computer's choice using a random number
    generator and if...else statements.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,20 @@
 //Need a way to initialize game
 //Computer makes its choice randomly
+function getComputerChoice() {
+    let selector = Math.floor(Math.random() * 100);
+    console.log(selector);
+    if (selector > 66) {
+        return 'rock';
+    } else if (selector < 33) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+getComputerChoice();
+console.log(getComputerChoice());
+
 //Receive user input (choice)
     //Must accept all spellings
 //Determine winner
