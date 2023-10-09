a2733ba62f5b6b566c6eefd132d188e3ef0a9fb3
Dexter Crago <jodieanddexter@gmail.com>
2023-10-09 11:48:41 -0700

    Declare whoWon and update playerSelection
    The whoWon function was declared but is not yet fully working. The idea
    is beginning to take shape.
    Functionality was added to the playerSelection function to display
    'Invalid choice' if 'rock', 'paper', or 'scissors' was not chosen. I do
    know that a loop would solve this problem better, however I have not
    gotten there yet in the curriculum and want to focus on solutions
    within my current understanding.

--- javascript.js
+++ javascript.js
@@ -10,14 +10,43 @@ function getComputerChoice() {
         return 'scissors';
     }
 }
+console.log('getComputerChoice:', getComputerChoice());
 //Receive user input (choice)
     //Must accept all spellings
 function playerSelection() {
     let playerChoice = prompt('Rock, Paper, or Scissors?');
     playerChoice = playerChoice.toLowerCase();
-    return playerChoice;
+    if (playerChoice != 'rock' && playerChoice != 'paper' && playerChoice != 'scissors') {
+        console.log("Invalid choice");
+    } else {
+        return playerChoice;
+    }
 }
+console.log('playerSelection:', playerSelection());
 //Determine winner
+function whoWon(getComputerChoice, playerSelection) {
+    let winner;
+    if (getComputerChoice === 'rock') {
+        if (playerSelection == 'rock') {
+            winner = 'tie';
+            return winner;
+        } else if (playerSelection == 'paper') {
+            winner = 'player';
+            return winner;
+        } else if (playerSelection == 'scissors') {
+            winner = 'computer';
+            return winner;
+        }
+    }
+    if (getComputerChoice === 'paper') {
+        return winner;
+    }
+    if (getComputerChoice === 'scissors') {
+        return winner;
+    }
+}
+console.log(whoWon());
+
 //Display who won
 //Need a way to play multiple times (five)
 //Need a system to keep track of scores
