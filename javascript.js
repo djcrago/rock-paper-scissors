337b4c2f55aa64c2ca6c52e2f4d94ee1f60febae
Dexter Crago <jodieanddexter@gmail.com>
2023-10-09 14:48:32 -0700

    Clean up code organization
    I wanted it to be obvious what is nested inside the overall game
    function.

--- javascript.js
+++ javascript.js
@@ -1,112 +1,104 @@
 //Need a way to initialize game
 function game() {
-//Computer makes its choice randomly
-function getComputerChoice() {
-    let selector = Math.floor(Math.random() * 100);
-    if (selector > 66) {
-        return 'Rock';
-    } else if (selector < 33) {
-        return 'Paper';
-    } else {
-        return 'Scissors';
-    }
-}
-const computerChoice = getComputerChoice();
-
-//Receive user input (choice)
-    //Must accept all spellings
-function playerSelection() {
-    let playerChoice = prompt('Rock, Paper, or Scissors?');
-    playerChoice = playerChoice.toLowerCase();
-    if (true) {
-        if (playerChoice == 'rock') {
-            playerChoice = 'Rock';
-        } else if (playerChoice =='paper') {
-            playerChoice = 'Paper';
+    //Computer makes its choice randomly
+    function getComputerChoice() {
+        let selector = Math.floor(Math.random() * 100);
+        if (selector > 66) {
+            return 'Rock';
+        } else if (selector < 33) {
+            return 'Paper';
         } else {
-            playerChoice = 'Scissors'
+            return 'Scissors';
         }
     }
-    if (playerChoice != 'Rock' && playerChoice != 'Paper' && playerChoice != 'Scissors') {
-        console.log("Invalid choice");
-    } else {
-        return playerChoice;
-    }
-}
-const playerChoice = playerSelection();
+    const computerChoice = getComputerChoice();
 
-//Determine winner
-function whoWon(computerChoice, playerChoice) {
-    function outcome(computerChoice, playerChoice) {
-        if (computerChoice == 'Rock') {
-            if (playerChoice == 'Rock') {
-                return 'tie';
-            } else if (playerChoice == 'Paper') {
-                return 'winner';
+    //Receive user input (choice)
+        //Must accept all spellings
+    function playerSelection() {
+        let playerChoice = prompt('Rock, Paper, or Scissors?');
+        playerChoice = playerChoice.toLowerCase();
+        if (true) {
+            if (playerChoice === 'rock') {
+                playerChoice = 'Rock';
+            } else if (playerChoice ==='paper') {
+                playerChoice = 'Paper';
             } else {
-                return 'loser';
-            }
-        } else if (computerChoice == 'Paper') {
-            if (playerChoice == 'Rock') {
-                return 'loser'; 
-            } else if (playerChoice == 'Paper') {
-                return 'tie'; 
-            } else {
-                return 'winner';
-            }
-        } else if (computerChoice == 'Scissors') {
-            if (playerChoice == 'Rock') {
-                return 'winner';
-            } else if (playerChoice == 'Paper') {
-                return 'loser';
-            } else {
-                return 'tie';
+                playerChoice = 'Scissors'
             }
         }
+        if (playerChoice != 'Rock' && playerChoice != 'Paper' && playerChoice != 'Scissors') {
+            console.log("Invalid choice");
+        } else {
+            return playerChoice;
+        }
     }
-    const outcometh = outcome(computerChoice, playerChoice);
-    if (outcometh == 'winner') {
-        return `You Win! ${playerChoice} beats ${computerChoice}`
-    } else if (outcometh == 'loser') {
-        return `You Lose! ${computerChoice} beats ${playerChoice}`
-    } else {
-        return "It's a tie!"
-    }
-}
-let result = whoWon(computerChoice, playerChoice);
-return result;
-}
+    const playerChoice = playerSelection();
 
-//Display who won
-// let gameOne = game();
-// console.log(gameOne);
+    //Determine winner
+    function whoWon(computerChoice, playerChoice) {
+        function outcome(computerChoice, playerChoice) {
+            if (computerChoice === 'Rock') {
+                if (playerChoice === 'Rock') {
+                    return 'tie';
+                } else if (playerChoice === 'Paper') {
+                    return 'winner';
+                } else {
+                    return 'loser';
+                }
+            } else if (computerChoice === 'Paper') {
+                if (playerChoice === 'Rock') {
+                    return 'loser'; 
+                } else if (playerChoice === 'Paper') {
+                    return 'tie'; 
+                } else {
+                    return 'winner';
+                }
+            } else if (computerChoice === 'Scissors') {
+                if (playerChoice === 'Rock') {
+                    return 'winner';
+                } else if (playerChoice === 'Paper') {
+                    return 'loser';
+                } else {
+                    return 'tie';
+                }
+            }
+        }
+        const outcometh = outcome(computerChoice, playerChoice);
+        if (outcometh === 'winner') {
+            return `You Win! ${playerChoice} beats ${computerChoice}`
+        } else if (outcometh === 'loser') {
+            return `You Lose! ${computerChoice} beats ${playerChoice}`
+        } else {
+            return "It's a tie!"
+        }
+    }
+    let result = whoWon(computerChoice, playerChoice);
+
+    //Display who won
+    console.log(result);
+    return result;
+    }
 
 //Need a way to play multiple times (five)
-// let gameTwo = game();
-// console.log(gameTwo);
-// let gameThree = game();
-// console.log(gameThree);
-// let gameFour = game();
-// console.log(gameFour);
-// let gameFive = game();
-// console.log(gameFive);
-for (let i = 0; i <= 4; i++) {
-    let gameResult = game();
-    console.log(gameResult);
+let gameResult;
+for (let i = 0; i < 2; i++) {
+    gameResult = game();
 }
 
 //Need a system to keep track of scores
-function scoreKeeper (gameOne) {
-    let scoreComputer = 0;
-    let scorePlayer = 0;
-    let resultNameOne = gameOne.substr(4, 1);
-    console.log(resultNameOne);
-    if (resultNameOne == 'W') {
-        scorePlayer++;
-    } else if (resultNameOne == 'L') {
-        scoreComputer++;
-    }
+let scoreComputer = 0;
+let scorePlayer = 0;
+let resultName = result.substr(4, 1);
+console.log(resultName);
+if (resultName === 'W') {
+    scorePlayer++;
+} else if (resultName === 'L') {
+    scoreComputer++;
 }
+console.log(scoreComputer);
+console.log(scorePlayer);
+
 
 //Determine overall winner
 
