1459e22323593ec4741c59fac78f8863f1c29a86
Dexter Crago <jodieanddexter@gmail.com>
2023-10-16 15:44:41 -0700

    Clean up code fluff and rename things for clarity
    All changes made were intended to shorten the code (make more efficient)
    or perform some task using less repetition and more automation

--- javascript.js
+++ javascript.js
@@ -1,20 +1,19 @@
-//Receive user input (choice)
-let playerChoice;
-let scoreComputer = 0;
-let scorePlayer = 0;
+//Step 1: Receive user input (choice)
 const rock = document.querySelector('#Rock');
 const paper = document.querySelector('#Paper');
 const scissors = document.querySelector('#Scissors');
 let choices = [rock, paper, scissors];
-let setChoice = choices.map((choice) => {
+let playerChoice;
+let getChoice = choices.map((choice) => { //add EventListener and set playerChoice in one step
     choice.addEventListener('click', () => {
         playerChoice = choice.getAttribute('id');
         playRound();
     });
 })
+let scoreComputer = 0; //if inside playRound they would be reset to 0 each click
+let scorePlayer = 0;
 function playRound() {
-    function getComputerChoice() {
-        //Computer makes its choice randomly
+    function getComputerChoice() { //computer makes its choice randomly
         let selector = Math.floor(Math.random() * 100);
         if (selector > 66) {
             return 'Rock';
@@ -24,10 +23,7 @@ function playRound() {
             return 'Scissors';
         }
     }
-    //Step 1: getComputerChoice
-    const computerChoice = getComputerChoice();
-    function whoWon(computerChoice, playerChoice) {
-        //Determine winner
+    function roundResult(computerChoice, playerChoice) { //determine winner
         if (computerChoice === 'Rock') {
             if (playerChoice === 'Rock') {
                 return 'tie';
@@ -48,50 +44,52 @@ function playRound() {
             } else return 'tie';
         }
     }
-    //Display winner
-    let resultBox = document.querySelector('#result');
-    const outcome = whoWon(computerChoice, playerChoice);
-        if (outcome === 'winner') {
-            scorePlayer++;
-            resultBox.textContent = `You Win! ${playerChoice} beats ${computerChoice}`;
-        } else if (outcome === 'loser') {
-            scoreComputer++;
-            resultBox.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`;
-        } else {
-            resultBox.textContent = `It's a tie between ${playerChoice.toLowerCase()}s!`;
-        }
+    //Step 2: getComputerChoice
+    const computerChoice = getComputerChoice();
+    //Step 3: Display winner of round
+    const outcome = roundResult(computerChoice, playerChoice);
+    let resultBox = document.querySelector('#roundResult');
+    if (outcome === 'winner') {
+        scorePlayer++;
+        resultBox.textContent = `You Win! ${playerChoice} beats ${computerChoice}`;
+    } else if (outcome === 'loser') {
+        scoreComputer++;
+        resultBox.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`;
+    } else {
+        resultBox.textContent = `It's a tie between ${playerChoice.toLowerCase()} and ${computerChoice.toLowerCase()}!`;
+    }
+    //Step 4: Update running score
     let runningScoreBox = document.querySelector('#runningScore');
-    runningScoreBox.textContent = `Player: ${scorePlayer} vs. Computer: ${scoreComputer}`;
-    let finalScoreBox = document.querySelector('#finalScore');
-    //The overall winner is the first to reach 5 points
-    if (scoreComputer >= 5 || scorePlayer >= 5) {
+    runningScoreBox.textContent = `Scores - Player: ${scorePlayer} vs. Computer: ${scoreComputer}`;
+    //Step 5: End game
+    if (scoreComputer >= 5 || scorePlayer >= 5) { //overall winner is first to reach 5 points
         let btns = document.querySelector('#btns');
-        btns.removeChild(rock);
-        btns.removeChild(paper);
-        btns.removeChild(scissors);
-        let finalScore = `Final Score - You: ${scorePlayer} Computer: ${scoreComputer}`;
-        let finalResult;
+        for (choice of choices) { //remove buttons to deny further playerChoices
+            btns.removeChild(choice);
+        }
+        //Step 6: Display winner of game
+        runningScoreBox.textContent = '';
+        let finalScoreBox = document.querySelector('#finalScore');
+        let finalScore = `Final Score - You: ${scorePlayer} vs. Computer: ${scoreComputer} - You `;
         if (scorePlayer > scoreComputer) {
-            finalResult = 'You Win!!!'
+            finalScore += 'Win!!!'
         } else {
-            finalResult = 'You Lost!'
+            finalScore += 'Lost!'
         }   
-        let finalMessage = finalScore + ' ' + finalResult;
-        finalScoreBox.textContent = finalMessage;
+        finalScoreBox.textContent = finalScore;
+        //Step 7: Reset game
         const newGame = document.createElement('button');
-        newGame.setAttribute('id', 'newGame');
         newGame.textContent = 'New Game'
         document.body.appendChild(newGame);
         newGame.addEventListener('click', () => {
             scorePlayer = 0;
             scoreComputer = 0;
             resultBox.textContent = '';
-            runningScoreBox.textContent = '';
             finalScoreBox.textContent = '';
+            for (choice of choices) {
+                btns.appendChild(choice);
+            }
             document.body.removeChild(newGame);
-            btns.appendChild(rock);
-            btns.appendChild(paper);
-            btns.appendChild(scissors);
         });
     }
 }
