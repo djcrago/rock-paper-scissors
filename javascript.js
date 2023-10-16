23f5b3d833fb27fdc1d9e3d11a8ac932596f82b7
Dexter Crago <jodieanddexter@gmail.com>
2023-10-16 12:11:27 -0700

    Display result in div using new logic
    Because playing a round is based on a button being pressed the outcome
    can be immediately displayed rather than needing to be passed along a
    few times.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 //Receive user input (choice)
 let playerChoice;
+let scoreComputer = 0;
+let scorePlayer = 0;
 const rock = document.querySelector('#rock');
     rock.addEventListener('click', () => {
         playerChoice = 'Rock';
@@ -31,51 +33,45 @@ function playRound() {
     const computerChoice = getComputerChoice();
     //Determine winner
     function whoWon(computerChoice, playerChoice) {
-        function outcome(computerChoice, playerChoice) {
-            if (computerChoice === 'Rock') {
-                if (playerChoice === 'Rock') {
-                    return 'tie';
-                } else if (playerChoice === 'Paper') {
-                    return 'winner';
-                } else {
-                    return 'loser';
-                }
-            } else if (computerChoice === 'Paper') {
-                if (playerChoice === 'Rock') {
-                    return 'loser'; 
-                } else if (playerChoice === 'Paper') {
-                    return 'tie'; 
-                } else {
-                    return 'winner';
-                }
-            } else if (computerChoice === 'Scissors') {
-                if (playerChoice === 'Rock') {
-                    return 'winner';
-                } else if (playerChoice === 'Paper') {
-                    return 'loser';
-                } else {
-                    return 'tie';
-                }
-            } 
-        }
-        const outcometh = outcome(computerChoice, playerChoice);
-        if (outcometh === 'winner') {
-            return `You Win! ${playerChoice} beats ${computerChoice}\n`
-        } else if (outcometh === 'loser') {
-            return `You Lose! ${computerChoice} beats ${playerChoice}\n`
+        if (computerChoice === 'Rock') {
+            if (playerChoice === 'Rock') {
+                return 'tie';
+            } else if (playerChoice === 'Paper') {
+                return 'winner';
+            } else {
+                return 'loser';
+            }
+        } else if (computerChoice === 'Paper') {
+            if (playerChoice === 'Rock') {
+                return 'loser'; 
+            } else if (playerChoice === 'Paper') {
+                return 'tie'; 
+            } else {
+                return 'winner';
+            }
+        } else if (computerChoice === 'Scissors') {
+            if (playerChoice === 'Rock') {
+                return 'winner';
+            } else if (playerChoice === 'Paper') {
+                return 'loser';
+            } else {
+                return 'tie';
+            }
+        } 
+    }
+    //Display winner
+    let resultBox = document.querySelector('#result');
+    const outcome = whoWon(computerChoice, playerChoice);
+        if (outcome === 'winner') {
+            scorePlayer++;
+            resultBox.textContent = `You Win! ${playerChoice} beats ${computerChoice}\n`;
+        } else if (outcome === 'loser') {
+            scoreComputer++;
+            resultBox.textContent = `You Lose! ${computerChoice} beats ${playerChoice}\n`;
         } else {
-            return "It's a tie!\n"
+            resultBox.textContent = "It's a tie!\n";
         }
-    }
-    let result = whoWon(computerChoice, playerChoice);
-    let scoreComputer = 0;
-    let scorePlayer = 0;
-    let resultName = result.substr(4, 2);
-    if (resultName === 'Wi') {
-        scorePlayer++;
-    } else if (resultName === 'Lo') {
-        scoreComputer++;
-    }
+    
     let finalScore = `Final Score\nYou: ${scorePlayer}\nComputer: ${scoreComputer}\n`;
     let finalResult;
     if (scorePlayer > scoreComputer) {
@@ -84,8 +80,7 @@ function playRound() {
         finalResult = 'You Lost!'
     }
     let finalMessage = result + finalScore + finalResult;
-    let resultBox = document.querySelector('#result');
-    resultBox.textContent = finalMessage;
+    
     console.log(finalMessage);
     }
 //Need a way to play multiple times (five)
